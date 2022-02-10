17a8653c8f251d05f8c0bfa9b33c10f04b553384
Rahul Dey <rdey71389@gmail.com>
2022-02-11 00:09:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import MainSec from './comps/MainSec';
+import Service from './comps/Service';
+import MobileApp from './comps/MobileApp';
+import FooterSec from './comps/FooterSec';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+    <MainSec />
+    <Service />
+    <MobileApp />
+    <FooterSec />
+    </>
+  );
+}
+
+export default App;
---
+++ src/DataArr.jsx
@@ -0,0 +1,55 @@
+const DataArr = [
+    {
+        city: "Ahmedabad",
+        link: "About us"
+    },
+    {
+        city: "Bangalore",
+        link: "Team"
+    },
+    {
+        city: "Chennai",
+        link: "Careers"
+    },
+    {
+        city: "Delhi",
+        link: "Foody Blog"
+    },
+    {
+        city: "Gurgaon",
+        link: "Foody Pop"
+    },
+    {
+        city: "Hyderabad",
+        link: "Foody Super"
+    },
+    {
+        city: "Kolkata",
+        link: "Help & Support"
+    },
+    {
+        city: "Mumbai",
+        link: "Partner with us"
+    },
+    {
+        city: "Pune",
+        link: "Ride with us"
+    },
+    {
+        link: "Terms & Conditions"
+    },
+    {
+        link: "Refund & Cancellation"
+    },
+    {
+        link: "Privacy Policy"
+    },
+    {
+        link: "Cookie Policy"
+    },
+    {
+        link: "Offer Terms"
+    }
+];
+
+export default DataArr;
